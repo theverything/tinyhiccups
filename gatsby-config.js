372a6611d98716dc9cd2be841774d96285606074
theverything <j3ffhorn@gmail.com>
2019-02-04 22:53:21 -0800

    add plugins to set html lang and pwa stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,25 @@ module.exports = {
     siteUrl: 'https://www.tinyhiccups.com',
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-html-attributes',
+      options: {
+        lang: 'en',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Tiny Hiccups',
+        short_name: 'Tiny Hiccups',
+        start_url: '/',
+        background_color: '#daacc9',
+        theme_color: '#daacc9',
+        display: 'standalone',
+        // icon: 'src/images/icon.png', // This path is relative to the root of the site.
+        // include_favicon: true, // Include favicon
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
@@ -34,5 +53,6 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
   ],
 };
